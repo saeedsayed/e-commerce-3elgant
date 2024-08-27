6d012ee52c8e6b40dc18e396961de15d65998dea
saeed pc <saeidsaued@gmail.com>
2024-08-28 02:42:26 +0300

    .

--- components/homePageComponents/newArrivals/newArrivals.tsx
+++ components/homePageComponents/newArrivals/newArrivals.tsx
@@ -1,12 +1,12 @@
 import NewArrivalsSlide from "./NewArrivalsSlide";
-import ArrowLink from "@/components/common/ArrowLink";
+import { ArrowLink } from "@/components/common";
 
 const NewArrivals = () => {
   return (
     <div className="container mt-12">
       <div className="flex justify-between mb-12 items-center">
         <h2 className="text-text text-2xl md:text-4xl font-bold">
-          New <br /> Arrivals
+          <p>New</p> Arrivals
         </h2>
         <ArrowLink href="shop">More Products</ArrowLink>
       </div>
