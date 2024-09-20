8a0cdbaedd57c1e2252c759787102b938a9d1228
saeed pc <saeidsaued@gmail.com>
2024-09-20 20:40:07 +0300

    '

--- components/homePageComponents/newArrivals/newArrivals.tsx
+++ components/homePageComponents/newArrivals/newArrivals.tsx
@@ -1,23 +1,24 @@
-import { getData } from "@/lib/getAPI";
-import NewArrivalsSlide from "./NewArrivalsSlide";
-import { ArrowLink } from "@/components/common";
-import { IProduct } from "@/types";
+// import { getData } from "@/lib/getAPI";
+// import NewArrivalsSlide from "./NewArrivalsSlide";
+// import { ArrowLink } from "@/components/common";
+// import { IProduct } from "@/types";
 
-const NewArrivals = async () => {
-  const [error, response]: [string | null, IProduct[]] = await getData("products", [
-    "thumbnail",
-    "review",
-  ]);
+const NewArrivals = () => {
+  // const [error, response]: [string | null, IProduct[]] = await getData("products", [
+  //   "thumbnail",
+  //   "review",
+  // ]);
   return (
-    <div className="container mt-12">
-      <div className="flex justify-between mb-12 items-center">
-        <h2 className="text-text text-2xl md:text-4xl font-bold">
-          <span className="block">New</span> Arrivals
-        </h2>
-        <ArrowLink href="shop">More Products</ArrowLink>
-      </div>
-      <NewArrivalsSlide data={response} />
-    </div>
+    // <div className="container mt-12">
+    //   <div className="flex justify-between mb-12 items-center">
+    //     <h2 className="text-text text-2xl md:text-4xl font-bold">
+    //       <p>New</p> Arrivals
+    //     </h2>
+    //     <ArrowLink href="shop">More Products</ArrowLink>
+    //   </div>
+    //   <NewArrivalsSlide data={response} />
+    // </div>
+    <h1>hello world</h1>
   );
 };
 
