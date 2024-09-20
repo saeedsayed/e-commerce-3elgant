3727816db8b5fbe4b7b0b9df67f5b53613d35fbf
saeed pc <saeidsaued@gmail.com>
2024-09-20 20:38:01 +0300

    '

--- components/homePageComponents/newArrivals/newArrivals.tsx
+++ components/homePageComponents/newArrivals/newArrivals.tsx
@@ -1,10 +1,10 @@
 import { getData } from "@/lib/getAPI";
 import NewArrivalsSlide from "./NewArrivalsSlide";
 import { ArrowLink } from "@/components/common";
-// import { IProduct } from "@/types";
+import { IProduct } from "@/types";
 
 const NewArrivals = async () => {
-  const [error, response]: [string | null, any[]] = await getData("products", [
+  const [error, response]: [string | null, IProduct[]] = await getData("products", [
     "thumbnail",
     "review",
   ]);
@@ -12,7 +12,7 @@ const NewArrivals = async () => {
     <div className="container mt-12">
       <div className="flex justify-between mb-12 items-center">
         <h2 className="text-text text-2xl md:text-4xl font-bold">
-          <p>New</p> Arrivals
+          <span className="block">New</span> Arrivals
         </h2>
         <ArrowLink href="shop">More Products</ArrowLink>
       </div>
